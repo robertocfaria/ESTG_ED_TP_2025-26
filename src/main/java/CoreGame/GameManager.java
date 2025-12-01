ec7069492daba2238537abfff007dff6638c83e9
8230067 <8230067@estg.ipp.pt>
2025-12-01 18:28:20 +0000

    menus e cenas

--- src/main/java/CoreGame/GameManager.java
+++ src/main/java/CoreGame/GameManager.java
@@ -1,4 +1,26 @@
 package CoreGame;
 
 public class GameManager {
+    //private List<IPlayer> players;
+
+
+    public void startGame() {
+
+    }
+
+    private void endTurn() {
+
+    }
+
+    private void swapPlayerPositions(IPlayer p1, IPlayer p2) {
+
+    }
+
+    private void shuffleAllPlayers() {
+
+    }
+
+
+
+
 }
